73447e3f3418ed6b9f72ddab1b7a07f315aff771
MagnusGreiff <mangegreiff@gmail.com>
2017-10-09 12:33:15 +0200

    Added support for markdown

--- src/Comment/HTMLForm/UpdateCommentForm.php
+++ src/Comment/HTMLForm/UpdateCommentForm.php
@@ -73,7 +73,10 @@ class UpdateCommentForm extends FormModel
         $comment = new Comment();
         $comment->setDb($this->di->get("db"));
         $comment->find("idcomment", $this->form->value("id"));
-        $comment->commenttext = $this->form->value("text");
+        $data = $this->form->value("text");
+        $text = $this->di->get("textfilter")->doFilter($data, ["bbcode",
+        "clickable", "shortcode", "markdown", "purify"]);
+        $comment->commenttext = $text;
         $id = $this->form->value("id");
         $comment->save("idcomment", $id);
         $id = $this->form->value("id");
