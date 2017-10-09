b8d46cadb1b2a22efe4f8e9f13686cf7a2ad91e3
MagnusGreiff <mangegreiff@gmail.com>
2017-10-09 14:05:25 +0200

    updated

--- view/users/editProfile.php
+++ view/users/editProfile.php
@@ -2,18 +2,6 @@
 
 namespace Anax\View;
 
-
-
-/**
- * View to create a new book.
- */
-// Show all incoming variables/functions
-//var_dump(get_defined_functions());
-//echo showEnvironment(get_defined_vars());
-
-// Create urls for navigation
-
-
 ?>
 <div class="wrapper"><h1>Update an item</h1>
     <?= $form ?>
