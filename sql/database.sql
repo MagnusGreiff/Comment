f7206ad4d68ff69b4900465531ba3286c34e79f7
MagnusGreiff <mangegreiff@gmail.com>
2017-10-22 19:10:06 +0200

    added more sql

--- sql/database.sql
+++ sql/database.sql
@@ -1,6 +1,10 @@
+DROP TABLE IF EXISTS CommentComments;
 DROP TABLE IF EXISTS Comments;
+DROP TABLE IF EXISTS Post2Cat;
+DROP TABLE IF EXISTS PostCategory;
 DROP TABLE IF EXISTS Posts;
 DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Book;
 
 
 /*USE anaxdb;*/
@@ -34,14 +38,79 @@ CREATE TABLE Comments (
 
 );
 
+CREATE TABLE CommentComments (
+    idcommentc INT NOT NULL AUTO_INCREMENT,
+    textcomment text(2499),
+    idcommentcomment INT,
+    postuser VARCHAR(100),
 
-INSERT INTO Posts (posttitle, postname, posttext) VALUE ("some title", "m@m.com", "blablablalbvlalfldspofksdpofksdop"),
-  ("post2", "hej@ööfld.com", "some post about something random");
+    PRIMARY KEY (idcommentc),
+
+    FOREIGN KEY (idcommentcomment) REFERENCES Comments (idcomment)
+);
+
+
+CREATE TABLE PostCategory (
+  id INT auto_increment,
+  category varchar(30),
+
+  primary key (id)
+);
+
+CREATE TABLE Post2Cat (
+  postid INT,
+  catid INT
+);
+
+
+
+INSERT INTO Posts (posttitle, postname, posttext) VALUE ("some title", "admin@admin.com", "blablablalbvlalfldspofksdpofksdop"),
+  ("post2", "admin@admin.com", "some post about something random");
 INSERT INTO Comments (commenttext, idpost, postuser)
-  VALUE ("jfsdsjifdsjifdsjifdjifdsio", "1", "some@f.com"), ("jfkldsjflksdjklfsqq", "1", "hej@d.com"),
-  ("fsdfsdf", "2", "l@lc.com"), ("fjdsoifjiosd", "2", "ööö@födö.com");
+  VALUE ("jfsdsjifdsjifdsjifdjifdsio", 1, "doe@doe.com"), ("jfkldsjflksdjklfsqq", 1, "doe@doe.com"),
+  ("fsdfsdf", 2, "admin@admin.com"), ("fjdsoifjiosd", 2, "admin@admin.com");
+INSERT INTO CommentComments (textcomment, idcommentcomment, postuser) VALUE ("hejsan men me", 1, "doe@doe.com"), ("men", 2, "admin@admin.com"), ("tjena", 2, "doe@doe.com"), ("jhejhejhejhejhejhejhejhejhejhej", 2, "admin@admin.com");
 
--- SELECT * FROM Comments;
+
+INSERT INTO PostCategory (category)
+VALUES  ("SQL"), ("PHP");
+
+INSERT INTO Post2Cat (postid, catid)
+VALUES (1,1), (2,2), (2,1);
+
+
+
+-- DROP VIEW IF EXISTS VPost;
+-- CREATE VIEW VPost as SELECT P.id as 'postid', P.posttitle as 'posttitle', P.posttext as 'posttext', P.postname as 'postname', group_concat(category) as Category FROM Posts as P
+--   INNER JOIN Post2Cat as P2C ON P.id = P2C.post_id
+--   INNER JOIN PostCategory as PC on PC.id = P2C.cat_id
+-- GROUP BY P.id ORDER BY P.id;
+
+
+DROP VIEW IF EXISTS VPost;
+CREATE VIEW VPost AS select p.id postid,
+  p.posttitle posttitle,
+  p.posttext posttext,
+  p.postname postname,
+  c.category Category
+  -- group_concat(c.category) gcategory
+from Posts p
+inner join Post2Cat p2c
+  on p.id = p2c.postid
+inner join PostCategory c
+  on p2c.catid = c.id;
+
+DROP VIEW IF EXISTS VCategory;
+CREATE VIEW VCategory as select p.id postid,
+  group_concat(c.category) Category
+  -- group_concat(c.category) gcategory
+from Posts p
+inner join Post2Cat p2c
+  on p.id = p2c.postid
+inner join PostCategory c
+  on p2c.catid = c.id
+  group by p.id order by p.id;
+
 
 
 DELIMITER //
@@ -70,13 +139,63 @@ CREATE PROCEDURE CheckComment(
 
 DELIMITER ;
 
--- SELECT P.id as `post id`, P.posttext as `Text`, P.postname AS `Name`, C.idcomment, C.commenttext, C.idpost FROM Comments as C
--- INNER JOIN Posts as P on C.idpost = P.id
--- GROUP BY C.idcomment;
+DELIMITER //
 
--- SELECT P.id, P.postname, P.posttext FROM Posts AS P
--- INNER JOIN Comments as C on P.id = C.idpost
--- GROUP BY P.id;
+DROP PROCEDURE IF EXISTS GetAllCommentsFromSpecific //
+CREATE PROCEDURE GetAllCommentsFromSpecific(
+    postuser VARCHAR(255)
+)
+  BEGIN
+    SELECT
+      C.idcomment,
+      C.commenttext,
+      C.postuser,
+      C.idpost
+    FROM Comments as C
+        INNER JOIN Posts as P ON C.idpost = P.id
+    WHERE C.postuser = postuser;
+    COMMIT;
+  END
+//
+
+DELIMITER ;
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS GetCommentsOfComments //
+CREATE PROCEDURE GetCommentsOfComments(
+    commentid INT
+)
+  BEGIN
+    SELECT
+      CC.idcommentc,
+      CC.textcomment,
+      CC.idcommentcomment,
+      CC.postuser
+    FROM CommentComments as CC
+        INNER JOIN Comments AS C ON CC.idcommentcomment = C.idcomment
+   WHERE C.idcomment = commentid;
+    COMMIT;
+  END
+//
+
+DELIMITER ;
+
+DELIMITER //
+
+--
+-- Table Book
+--
+
+CREATE TABLE Book (
+  `id`       INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  `title`    VARCHAR(256)                       NOT NULL,
+  `author`   VARCHAR(256)                       NOT NULL,
+  `released` DATE                               NOT NULL
+)
+  ENGINE INNODB
+  CHARACTER SET utf8
+  COLLATE utf8_swedish_ci;
 
 
 CREATE TABLE Users (
@@ -85,6 +204,7 @@ CREATE TABLE Users (
   `email`       VARCHAR(100)                       NOT NULL UNIQUE,
   `age`         INTEGER(3)                         NOT NULL,
   `password`    VARCHAR(255)                       NOT NULL,
+  `points`      INT DEFAULT '0'                            ,
   `permissions` VARCHAR(5) DEFAULT 'user',
   `created`     DATETIME,
   `updated`     DATETIME,
